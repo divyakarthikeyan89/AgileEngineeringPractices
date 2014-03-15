abcf23d69ead2e07b2266e0654dc133242ba17f4
divyakarthikeyan89 <divyak@ischool.berkeley.edu>
2014-03-15 15:53:20 -0700

    Create Scalar.java

---
+++ i290t-aep/src/main/edu/berkeley/ischool/aep/Scalar.java
@@ -0,0 +1,10 @@
+package edu.berkeley.ischool.aep;
+
+/**
+ * Created by divyakarthikeyan on 2/21/14.
+ */
+public class Scalar extends Quantity{
+    public Scalar (Unit unit, double value){
+        super(unit,value);
+    }
+}
